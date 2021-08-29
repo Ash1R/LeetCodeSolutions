e9d216bd2fe0d4172e2448ee3d3f4d5674e9ecc0
Ashir <ashir.rao26@gmail.com>
2021-08-29 10:30:11 -0700

    Update Solutiontwo.java

--- LC227/Solutiontwo.java
+++ LC227/Solutiontwo.java
@@ -1,65 +1,68 @@
 import java.util.*;
 
+//comment
 class Solution {
     public int calculate(String s) {
-        
-        StringTokenizer st = new StringTokenizer(s, "+-*/ " , true);
+
+        StringTokenizer st = new StringTokenizer(s, "+-*/ ", true);
         List<String> passone = new ArrayList<>();
-        while(st.hasMoreTokens()){
+        while (st.hasMoreTokens()) {
             String temp = st.nextToken();
-            if (!temp.equals(" ")){
+            if (!temp.equals(" ")) {
                 passone.add(temp);
             }
         }
-        if (passone.size() == 1){
+        if (passone.size() == 1) {
             return Integer.parseInt(passone.get(0));
         }
         boolean lastadd = true;
         int dump = -1;
         boolean lastmult = false;
         List<String> passtwo = new ArrayList<>();
-        for (int i = 0; i < passone.size(); i++){
-            if (passone.get(i).equals("+") || passone.get(i).equals("-")){
-                if (dump != -1){
-                passtwo.add(String.valueOf(dump));}
-                if (lastadd){
-                passtwo.add(passone.get(i - 1));}
+        for (int i = 0; i < passone.size(); i++) {
+            if (passone.get(i).equals("+") || passone.get(i).equals("-")) {
+                if (dump != -1) {
+                    passtwo.add(String.valueOf(dump));
+                }
+                if (lastadd) {
+                    passtwo.add(passone.get(i - 1));
+                }
                 passtwo.add(passone.get(i));
-                if (i+1 == passone.size() - 1){
-                    passtwo.add(passone.get(i+1));
+                if (i + 1 == passone.size() - 1) {
+                    passtwo.add(passone.get(i + 1));
                 }
                 lastadd = true;
                 lastmult = false;
                 dump = -1;
-            } else if (passone.get(i).equals("*")){
-                if (!lastmult){
-                dump = (Integer.parseInt(passone.get(i-1)) * Integer.parseInt(passone.get(i+1)));
-                lastmult = true;
-                } else{
-                    dump *= Integer.parseInt(passone.get(i+1));
+            } else if (passone.get(i).equals("*")) {
+                if (!lastmult) {
+                    dump = (Integer.parseInt(passone.get(i - 1)) * Integer.parseInt(passone.get(i + 1)));
+                    lastmult = true;
+                } else {
+                    dump *= Integer.parseInt(passone.get(i + 1));
                 }
                 lastadd = false;
-            } else if (passone.get(i).equals("/")){
-                if (!lastmult){
-                dump = (Integer.parseInt(passone.get(i-1)) / Integer.parseInt(passone.get(i+1)));
-                lastmult = true;
-                } else{
-                    dump = dump / Integer.parseInt(passone.get(i+1));
+            } else if (passone.get(i).equals("/")) {
+                if (!lastmult) {
+                    dump = (Integer.parseInt(passone.get(i - 1)) / Integer.parseInt(passone.get(i + 1)));
+                    lastmult = true;
+                } else {
+                    dump = dump / Integer.parseInt(passone.get(i + 1));
                 }
                 lastadd = false;
             }
         }
         passtwo.add(String.valueOf(dump));
 
-        //System.out.println(passtwo);
+        // System.out.println(passtwo);
         int answer = 0;
         answer += Integer.parseInt(passtwo.get(0));
-        for (int i = 0; i < passtwo.size(); i++){
-            if (i % 2 == 1){
-                if (passtwo.get(i).equals("+")){
-                    answer += Integer.parseInt(passtwo.get(i+1));
-                } else if (passtwo.get(i).equals("-")){
-                    answer -= Integer.parseInt(passtwo.get(i+1));
+        for (int i = 0; i < passtwo.size(); i++) {
+            if (i % 2 == 1) {
+                if (passtwo.get(i).equals("+")) {
+                    answer += Integer.parseInt(passtwo.get(i + 1));
+                } else if (passtwo.get(i).equals("-")) {
+                    answer -= Integer.parseInt(passtwo.get(i + 1));
                 }
             }
         }
